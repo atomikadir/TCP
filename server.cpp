348ea623d9723d37c0943c31f5a5bb0fc3a8390a
Abdulkadir <101278138+atomikadir@users.noreply.github.com>
2023-06-04 17:49:47 +0300

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,24 +1,28 @@
 #include <WinSock2.h>
-#include <ws2tcpip.h>
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include <mutex>
 using namespace std;
 
-#pragma comment(lib,"ws2_32.lib")
+#pragma comment(lib,"ws2_32.lib") 
 #pragma warning(disable:4996)
 
 #define BUFLEN 1024
 #define PORT 6161
-#define SERVER "127.0.0.1"
+
+void sendd(SOCKET* atom_client, mutex* mtx);
+void recvv(SOCKET* atom_client, mutex* mtx);
 
 int main() {
-	system("title TCP Server");
+	system("title TCP Server"); // cmd basligi degistirir.
 
 	WSADATA wsaData;
-	
+	// winsok baslatilir.
 	int iResult;
 	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
 	if (iResult != 0) {
-		printf("WSAStartup failed. Hata kodu:%d\n",WSAGetLastError());
+		printf("WSAStartup failed. Hata kodu:%d\n", WSAGetLastError());
 		return 1;
 	}
 	printf("WSAStartup Success.\n");
@@ -32,7 +36,7 @@ int main() {
 		return 1;
 	}
 	printf("soket olusturuldu.\n");
-	
+
 	//server ayarlari
 	sockaddr_in server;
 	server.sin_addr.s_addr = INADDR_ANY;
@@ -47,8 +51,8 @@ int main() {
 		return 1;
 	}
 	printf("baglandi.\n");
-	
-	//listen
+
+	//listen handshake 1
 	if (listen(atom_server, 1) == SOCKET_ERROR) {
 		printf("dinleme basarisiz. Hata kodu:%d\n", WSAGetLastError());
 		closesocket(atom_server);
@@ -62,7 +66,7 @@ int main() {
 	int clientLength = sizeof(client);
 
 	printf("baglanti bekleniyor..\n");
-
+	// handshake 3. baglanan client'ı kabul eder.
 	atom_client = accept(atom_server, (sockaddr*)&client, &clientLength);
 	if (atom_client == INVALID_SOCKET) {
 		printf("baglanamadi.Hata kodu:%d\n", WSAGetLastError());
@@ -71,42 +75,58 @@ int main() {
 		return 1;
 	}
 	printf("kabul edildi.\n");
-	//ntohs(client.sin_port);
 
-	bool running = true;
+	mutex mtx;
 
-	while (true) {
-		char message[BUFLEN] = {};
-		//memset(message, 0, sizeof(message));
-		// gelen cevap recvfrom ile alinir. yanit gelene kadar program burada bekler
-		if (recv(atom_client, message, sizeof(message)-1,0 ) == SOCKET_ERROR) {
-			printf("mesaj alinamadi. Hata kodu: %d", WSAGetLastError());
-			exit(0);
-		}
-		printf("Client: %s\n", message);
-		printf("Server:");
+	thread first(recvv, &atom_client, &mtx);
+	thread second(sendd, &atom_client, &mtx);
 
-		char answer[BUFLEN] = {};
-		// g�nderilecek mesaj kullanicidan alinir.
-		cin.getline(answer, BUFLEN);
+	first.join();
+	second.join();
 
-		// eger cikis yapilmak istenirse 'q' yollanir. 
-		if (strcmp(message, "q") == 0)
-		{
-			running = false;
-			continue;
-		}
-
-		//kullanicidan alinan mesaj g�nderilir.
-		if (send(atom_client, answer, strlen(answer), 0) == SOCKET_ERROR) {
-			printf("mesaj gonderilemedi. Hata kodu: %d", WSAGetLastError());
-			return 1;
-		}
-	}
 	//istemci baglantisi kapat
 	closesocket(atom_server);
 	closesocket(atom_client);
 	WSACleanup();
 
 	return 0;
+}
+
+void sendd(SOCKET* atom_client, mutex* mtx) {
+	bool running = true;
+	while (running) {
+		char answer[BUFLEN] = {};
+
+		printf("Server:");
+		// gönderilecek mesaj kullanicidan alinir.
+		cin.getline(answer, BUFLEN);
+
+		// eger cikis yapilmak istenirse 'q' yollanir. 
+		if (strcmp(answer, "quit") == 0)
+		{
+			running = false;
+			WSACleanup();
+			closesocket(*atom_client);
+			continue;
+		}
+
+		//kullanicidan alinan mesaj gönderilir.
+		if (send(*atom_client, answer, strlen(answer) + 1, 0) == SOCKET_ERROR) {
+			printf("mesaj gonderilemedi. Hata kodu: %d", WSAGetLastError());
+		}
+	}
+}
+void recvv(SOCKET* atom_client, mutex* mtx) {
+
+	// mesaj alıp gonderme
+	while (true) {
+		char message[BUFLEN] = {};
+
+		// gelen cevap recvfrom ile alinir. yanit gelene kadar program burada bekler
+		if (recv(*atom_client, message, sizeof(message) - 1, 0) == SOCKET_ERROR) {
+			printf("mesaj alinamadi. Hata kodu: %d", WSAGetLastError());
+		}
+		printf("Client: %s\n", message);
+		printf("Server:");
+	}
 }
